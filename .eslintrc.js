699d07e0d7b15fb9ac0ac6db4a293090141643d8
Red Markham <redmark@netzero.net>
2019-01-30 17:37:13 -0500

    Add gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,39 @@
 module.exports = {
-  "env": {
-      "browser": true,
-      "commonjs": true,
-      "es6": true,
-      "node": true
-  },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-      "ecmaVersion": 6,
-      "sourceType": "module"
-  },
-  "rules": {
-    "no-console":0,
-      "indent": [
-          "error",
-          2
-      ],
-      "quotes": [
-          "error",
-          "single"
-      ],
-      "semi": [
-          "error",
-          "always"
-      ]
-  }
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "airbnb-base",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "rules": {
+        "prefer-template": "warn",
+        "no-unused-vars" : ["error", { "vars": "local", "args" : "none" }],
+        "import/newline-after-import": ["warn", { "count": 1 }],
+        "object-curly-spacing": ["warn", "always"],
+        "indent": ["warn", 2],
+        "comma-dangle": ["error", "only-multiline"],
+        "consistent-return": ["off"],
+        "func-names": ["error", "never"],
+        "global-require": ["off"],
+        "max-len": ["off"],
+        "no-console": ["off"],
+        "no-param-reassign": ["error", {
+        "props": false
+        }],
+        "no-underscore-dangle": ["off"],
+        "prefer-arrow-callback": ["off", {
+        "allowNamedFunctions": true,
+        "allowUnboundThis": true
+        }],
+        "prefer-destructuring": ["off"],
+        "space-before-function-paren": ["off"]
+    }
 };
